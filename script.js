332762b7c299e2ba2e64f4f9badb39ba88177184
EkaterinaGrishel <katesobol88@gmail.com>
2022-11-17 02:55:31 +0300

    Домашнее задание 10

--- script.js
+++ script.js
@@ -5,4 +5,33 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // Здесь твой код
+  const name = form.querySelector("#name").value;
+  const secondName = form.querySelector("#secondName").value;
+  const email = form.querySelector("#email").value;
+  const phone = form.querySelector("#phone").value;
+
+  // fetch('http://46.21.248.81:3001/user'
+  fetch('https://polinashneider.space/', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      secondName,
+      email,
+      phone
+    })
+  })
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      alert(data);
+      form.reset();
+    })
+    .catch((error) => {
+      alert(error);
+    })
+
 });
